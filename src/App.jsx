413df20e267da202abc395ad5b28f89e5efcc606
Yuval207 <yuvalsharma49@gmail.com>
2024-02-22 02:33:59 +0530

    created ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,28 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 
 function App() {
-  return <div></div>;
+  const divRef = useRef();
+  const [length, setLength] = useState(8);
+  const handleLength = (e) => {
+    setLength(e.target.value);
+  };
+  return (
+    <div>
+      <h1>Password Generator</h1>
+      <input type="text" />
+      <button>Copy</button>
+      <label>
+        <input type="checkbox" />
+        Numbers
+      </label>
+      <label>
+        <input type="checkbox" />
+        Symbols
+      </label>
+      <p>Length : {length}</p>
+      <input type="range" min={8} max={20} onChange={handleLength} />
+    </div>
+  );
 }
 
 export default App;
